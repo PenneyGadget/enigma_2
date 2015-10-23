b0195b66e1ce9e540e3888dfe12c060e2a577f0a
PenneyGadget <dreamofgreen@gmail.com>
2015-10-23 17:19:50 -0600

    key class functioning properly

--- lib/key.rb
+++ lib/key.rb
@@ -1,6 +1,12 @@
 
 class Key
 
+  attr_accessor :key  # => nil
+
+  def initialize
+    @key = generate_key
+  end
+
   def generate_key
     key = rand(0..99999).to_s
     if key.length < 5
@@ -10,10 +16,14 @@ class Key
     end
   end
 
-  def create_rotations
-
+  def key_rotations
+    a = key[0].to_s + key[1].to_s
+    b = key[1].to_s + key[2].to_s
+    c = key[2].to_s + key[3].to_s
+    d = key[3].to_s + key[4].to_s
+    rotations = [a, b, c, d].map do |r|
+      r.to_i
+    end
   end
-
-
 
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -5,14 +5,21 @@ require '../lib/key'
 class KeyTest < Minitest::Test
 
   def test_the_key_generates_a_random_five_digit_number
-    key_one = Key.new.generate_key
-    key_two = Key.new.generate_key
+    key_one = Key.new.key
+    key_two = Key.new.key
 
     1000.times do
       refute_equal key_one, key_two
     end
 
     assert_equal 5, key_one.length
+  end
+
+  def test_the_rotations_are_properly_generated_from_a_key
+    skip
+    key = "32879"
+
+    assert_equal [32, 28, 87, 79], key.key_rotations
   end
 
 
