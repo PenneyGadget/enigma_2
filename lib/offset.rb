402929c48f99702f3e282851709c2e742c7ad86d
PenneyGadget <dreamofgreen@gmail.com>
2015-10-23 17:38:08 -0600

    offset and offset tests

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,28 @@
 require 'date'
+require 'pry'
 
 class Offset
 
+  def initialize
+    @date = date
+  end
 
+  def date
+    Date.today.strftime("%d%m%y")
+  end
+
+  def offset
+    (date.to_i ** 2).to_s[-4..-1]
+  end
+
+  def offset_rotations
+    a = offset[0]
+    b = offset[1]
+    c = offset[2]
+    d = offset[3]
+    rotations = [a, b, c, d].map do |r|
+      r.to_i
+    end
+  end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -4,6 +4,22 @@ require '../lib/offset'
 
 class OffsetTest < Minitest::Test
 
+  def test_date_exists_and_is_formatted_correctly
+    offset = Offset.new
 
+    assert_equal "231015", offset.date
+  end
+
+  def test_proper_offset_is_generated_from_the_date
+    offset = Offset.new
+
+    assert_equal "0225", offset.offset
+  end
+
+  def test_offset_rotations_are_generated_properly
+    offset = Offset.new
+
+    assert_equal [0, 2, 2, 5], offset.offset_rotations
+  end
 
 end
