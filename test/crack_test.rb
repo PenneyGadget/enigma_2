2bf37369b3ab294b2fee552b694155668345b1af
PenneyGadget <dreamofgreen@gmail.com>
2015-10-27 16:21:45 -0600

    updated crack for expanded character map

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -15,41 +15,39 @@ class CrackTest < Minitest::Test
   def test_we_have_the_character_map
     c = Crack.new("blarg")
 
-    assert_equal ('a'..'z').to_a + ('0'..'9').to_a + [" ", ".", ","], c.character_map
+    assert_equal (' '..'z').to_a, c.character_map
   end
 
   def test_we_have_end_position_information_based_on_message_length
-    c = Crack.new("t3iw0w8jq,ajk")
+    c = Crack.new("Xi:Xp]0u8S':8_,8UZuN7S#8NM)OOi:XX")
 
     assert_equal 1, c.message.length % 4
-    # why don't these work?
-    # assert_equal @end_remainder_one, c.end_position
-    # assert_equal [4, 13, 3, 37], @end_remainder_one
+    assert_equal [69, 78, 68, 14], c.end_remainder_one
   end
 
   def test_we_have_a_temp_end_piece_of_message_to_work_with
-    c = Crack.new("t3iw0w8jq,ajk")
+    c = Crack.new("Xi:Xp]0u8S':8_,8UZuN7S#8NM)OOi:XX")
 
     assert_equal 1, c.message.length % 4
-    assert_equal ["q", ",", "a", "j"], c.temp_message
+    assert_equal ["O", "i", ":", "X"], c.temp_message
   end
 
   def test_we_have_the_character_map_position_of_that_end_piece
-    c = Crack.new("t3iw0w8jq,ajk")
+    c = Crack.new("Xi:Xp]0u8S':8_,8UZuN7S#8NM)OOi:XX")
 
-    assert_equal [16, 38, 0, 9], c.key_text_position
+    assert_equal [47, 73, 26, 56], c.key_text_position
   end
 
   def test_we_can_then_find_the_true_key
-    c = Crack.new("t3iw0w8jq,ajk")
+    c = Crack.new("Xi:Xp]0u8S':8_,8UZuN7S#8NM)OOi:XX")
 
-    assert_equal [12, 25, -3, -28], c.find_key
+    assert_equal [-22, -5, -42, 42], c.find_key
   end
 
   def test_decryption_works
-    c = Crack.new("t3iw0w8jq,ajk")
+    c = Crack.new("6CN\\N7Dyq-;>q9@<34.Rp-7<,'=S-CN\\69R`q,-`:")
 
-    assert_equal "hello ..end..", c.decrypt
+    assert_equal "****By George we've done it!!**** ..end..", c.decrypt
   end
 
 end
