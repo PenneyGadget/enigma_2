4192b279fd8b94f27aff94657903e89380ceff1c
PenneyGadget <dreamofgreen@gmail.com>
2015-10-27 13:54:07 -0600

    created enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,6 +1,21 @@
+# require_relative 'key'
+# require_relative 'offset'
+require_relative 'encrypt'
+require_relative 'decrypt'
+require_relative 'crack'
 
 class Enigma
 
+  def encrypt(message, key = nil, date = nil)
+    Encrypt.new(message, key, date).encrypt
+  end
 
+  def decrypt(message, key, date = nil)
+    Decrypt.new(message, key, date).decrypt
+  end
+
+  def crack(message, date = nil)
+    Crack.new(message, date = nil).decrypt
+  end
 
 end
