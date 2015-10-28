0c29912333ea23a4d1e8e1b5e5a5caa0f7e8b1cb
PenneyGadget <dreamofgreen@gmail.com>
2015-10-27 19:04:11 -0600

    enigma and enigma tests fixed for expanded character map

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,11 @@
-require_relative 'encrypt'
-require_relative 'decrypt'
-require_relative 'crack'
+require_relative 'encrypt'  # => true
+require_relative 'decrypt'  # => true
+require_relative 'crack'    # => true
 
 class Enigma
 
   def encrypt(message, key = nil, date = nil)
-    Encrypt.new(message, key, date).encrypt
+    Encrypt.new(message, key, date).encrypt    # => "ufE[3VHgFmPtSVCOSz"
   end
 
   def decrypt(message, key, date = nil)
@@ -17,3 +17,6 @@ class Enigma
   end
 
 end
+
+e = Enigma.new                          # => #<Enigma:0x007fd540941200>
+e.encrypt("Puppies!!! ..end..", 37465)  # => "ufE[3VHgFmPtSVCOSz"
